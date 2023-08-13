9ba6773d872792e98d60e2bc26453bf50d5c233b
TURAL10 <124426123+TURAL10@users.noreply.github.com>
2023-08-13 04:17:25 +0400

    Add project files.

---
+++ task13.08/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using task13._08.Models;
+
+namespace task13._08.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private List<Student> students = new List<Student>()
+        {
+            new(){Id = 1,Name="Messi",Age=36},
+            new(){Id = 2,Name="Ronaldo",Age=38},
+            new(){Id = 3,Name="Neymar",Age=31},
+
+
+        };
+        
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+        public IActionResult About(int id)
+        {
+            return View(students.Find(s => s.Id == id));
+        }
+    }
+}
---
+++ task13.08/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace task13._08.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public int Age { get; set; }
+	}
+}
